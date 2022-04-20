2ce604b7a7a7ebb1514dd2f316a0ada6a3a947ff
Harshith Kumar H <harshith.k@prixgen.com>
2022-04-20 16:32:38 +0530

    quality_control_base

---
+++ quality_control_base/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Quality Control Base",
+
+    'summary': """
+        Quality Base
+        """,
+
+    'description': """
+Define quality points that will generate quality checks on pickings,
+manufacturing orders or work orders (quality_mrp),
+Quality alerts can be created independently or related to quality checks,
+Possibility to add a measure to the quality check with a min/max tolerance,
+Define your stages for the quality alerts.
+        """,
+
+    'author': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'website': 'https://www.prixgen.com',
+    'company': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'license': 'LGPL-3',
+    
+    'category': 'Quality',
+    'version': '15.0.0.2_Beta',
+    'depends': ['base','stock','product','quality','quality_control','mrp','quality_mrp','product_expiry','quality_control_worksheet'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/automation.xml',
+        'views/views.xml',
+    ],
+}
