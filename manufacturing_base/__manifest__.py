530534afc64d278875beb177dba44151016fdde0
Harshith Kumar H <harshith.k@prixgen.com>
2022-04-29 10:43:09 +0530

    manufacturing_base

--- manufacturing_base/__manifest__.py
+++ manufacturing_base/__manifest__.py
@@ -14,7 +14,7 @@
     'website': "https://www.prixgen.com",
 
     'category': 'Manufacturing',
-    'version': '15.0.2.9_Beta',
+    'version': '15.0.3.0_Beta',
     'license': 'LGPL-3',
 
     'depends': ['mrp','base','sale','mrp_account_enterprise','mrp_subcontracting','inventory_base'],
