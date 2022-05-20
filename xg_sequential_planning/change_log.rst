cdd9b865e0e9d6e7882890f5895461675516e619
Harshith Kumar H <harshith.k@prixgen.com>
2022-05-20 11:38:10 +0530

    sequential_planning

--- xg_sequential_planning/change_log.rst
+++ xg_sequential_planning/change_log.rst
@@ -9,3 +9,7 @@ updated sequential planning code for malti mo's
 15.0.0.2 --------> 15.0.0.3(4-May-2022) **Anke
 ================================================
 Change sequential planning based on Float Value
+
+15.0.0.3 --------> 15.0.0.4(19-May-2022) **Anke
+================================================
+Avoid the updation of Date for last child
--- xg_sequential_planning/models/sequential_planning.py
+++ xg_sequential_planning/models/sequential_planning.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import AccessError, UserError, ValidationError
-
+from itertools import chain
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
@@ -44,32 +44,65 @@ class MrpProduction(models.Model):
                 mo_id = self.env['mrp.production'].search([('id','=',line)])
                 if mo_id.bom_id.xg_activate_sequential_planning == True:
                     mo_list.append(mo_id)
+            
+            # split_sequence Code Start
             float_list = []
             for x in mo_list:
                 if x.split_sequence not in float_list:
                     float_list.append(x.split_sequence) 
+            float_list.sort()
             float_list.sort(reverse=True)
-            for float_value in float_list:
+
+            
+            value = int(float_list[0])
+            values = []
+            for l in range(value+1):
+                if l != 0:
+                    values.append(l)
+            values.sort(reverse=True)
+            created_list = []
+            for new_val in values:
+                new_list = self.split_at_values(new_val,float_list)
+                if len(new_list):
+                    created_list.append(new_list)
+
+            value = 0
+            new_sequence_list = []
+            for list1 in range(0,len(created_list[0])):
+                new_sequence_list = new_sequence_list + [item[value]  for item in created_list[:-1]]
+                value+=1
+            
+            
+            list_count = len(created_list)-2
+            new_sequence_list.append(float_list[-1])
+            # split_sequence Code End
+
+            for float_value in new_sequence_list:
                 for mos in mo_list:                
                     if mos.split_sequence == float_value:
                         if mos.workorder_ids:
                             mos._context.update({'change_dead_line':True})
                             mos.button_plan()
-                            if float_value != float_list[-1]:
+                            if float_value != new_sequence_list[-1]:
                                 for search_mo in mo_list:
-                                    if search_mo.split_sequence == float_list[float_list.index(float_value)+1]:
-                                        if search_mo.state == 'confirmed':
-                                            search_mo.write({'date_planned_start' :max(mos.workorder_ids.mapped('date_planned_finished'))})
-                                
-
-
-
-
-        # def button_plan(self):
-        # """ Create work orders. And probably do stuff, like things. """
-        # orders_to_plan = self.filtered(lambda order: not order.is_planned)
-        # orders_to_confirm = orders_to_plan.filtered(lambda mo: mo.state == 'draft')
-        # orders_to_confirm.action_confirm()
-        # for order in orders_to_plan:
-        #     order._plan_workorders()
-        # return True
+                                    if int(float_value) != int(new_sequence_list[-2]):
+                                        if search_mo.split_sequence == new_sequence_list[new_sequence_list.index(float_value)+1]:
+                                            if search_mo.state == 'confirmed':
+                                                search_mo.write({'date_planned_start' :max(mos.workorder_ids.mapped('date_planned_finished'))})
+                                    if float_value == new_sequence_list[-2]:
+                                        if search_mo.split_sequence == new_sequence_list[-1]:
+                                            if search_mo.state == 'confirmed':
+                                                search_mo.write({'date_planned_start' :max(mos.workorder_ids.mapped('date_planned_finished'))})
+                                                
+        
+    def split_at_values(self,number, values):
+        first_val = int(values[0])
+        new_list_list = []
+        for value in values:
+            if number == first_val:
+                if value >= number:
+                    new_list_list.append(value)
+            else:
+                if (number+1) > value >= number:
+                    new_list_list.append(value)
+        return new_list_list
